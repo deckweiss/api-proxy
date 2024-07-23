3c9acb28b5ac8b62edc0890c0683b535092663e0
Alexander Betz <alexander.betz@deckweiss.at>
2024-07-23 14:31:02 +0200

    chore: moved local repo to github

---
+++ src/lib/proxy.ts
@@ -0,0 +1,38 @@
+import { RequestEvent, type Handle } from '@sveltejs/kit';
+
+interface Options {
+    apiUrl: string;
+    basePath: string;
+    middleware?: (event: RequestEvent) => void;
+}
+
+export function createApiProxy(options: Options) {
+    const handleApiProxy: Handle = async ({ event, resolve }) => {
+        if (event.url.pathname.startsWith(options.basePath)) {
+            // build the new URL path with your API base URL, the stripped path and the query string
+            const urlPath = `${options.apiUrl}${event.url.pathname}${event.url.search}`;
+            const proxiedUrl = new URL(urlPath);
+
+            // Strip off header added by SvelteKit yet forbidden by underlying HTTP request
+            // library `undici`.
+            // https://github.com/nodejs/undici/issues/1470
+            event.request.headers.delete('connection');
+
+            if (options.middleware) {
+                options.middleware(event);
+            }
+
+            return fetch(proxiedUrl.toString(), {
+                // propagate the request method and body
+                body: event.request.body,
+                method: event.request.method,
+                headers: event.request.headers
+            }).catch((err) => {
+                console.log('Could not proxy API request: ', err);
+                throw err;
+            });
+        }
+        return resolve(event);
+    };
+    return handleApiProxy;
+}
